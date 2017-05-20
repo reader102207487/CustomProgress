3a365ac095fab138ed7d40015cd56cac482d1b29
reader102207487 <qff_it@163.com>
2017-05-20 11:06:41 +0800

    Fist Commit

---
+++ app/src/main/java/com/qffweather/android/hyzingdingyi_three/MainActivity.java
@@ -0,0 +1,13 @@
+package com.qffweather.android.hyzingdingyi_three;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
